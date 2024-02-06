444b3d011b20334ab633e1157dec56ef7ece69db
MikeAlexandre <mikealexandreee97@gmail.com>
2024-02-06 18:56:01 -0300

    bitwise

---
+++ UpCase.java
@@ -0,0 +1,15 @@
+class UpCase {
+    public static void main(String[] args) {
+
+        char ch;
+        for(int i = 0; i < 10; i ++) {
+            ch = (char) ('a' + i);
+            System.out.print(ch);
+
+            // Essa instrução desativa o 6º bit
+            ch = (char) ((int) ch & 65503); // agora ch é maiúscula
+
+            System.out.print(ch + " ");
+        }
+    }
+}
